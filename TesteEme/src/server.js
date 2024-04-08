cd7cf864020f72ebe50556121626f86e9356a4d0
EmersonSantana1994 <emerson1994vp@gmail.com>
2024-04-08 20:26:45 -0300

    funcionar com https

--- TesteEme/src/server.js
+++ TesteEme/src/server.js
@@ -1,16 +1,17 @@
-require('dotenv').config({path:'variaveis.env'});
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// require('dotenv').config({path:'variaveis.env'});
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
 
-const routes = require('./routes');
 
-const server = express();
-server.use(cors());
-server.use(bodyParser.urlencoded({extended: false}));
-// server.use('/api', routes);
+// const routes = require('./routes');
 
-server.listen(process.env.PORT,()=>{
-    console.log("?????", process.env.PORT)
-    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
-});
+// const server = express();
+// server.use(cors());
+// server.use(bodyParser.urlencoded({extended: false}));
+// // server.use('/api', routes);
+
+// server.listen(process.env.PORT,()=>{
+//     console.log("?????", process.env.PORT)
+//     console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
+// });
