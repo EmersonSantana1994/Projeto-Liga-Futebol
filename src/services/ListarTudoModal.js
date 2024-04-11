dc67921e763c1d17712185d8f75921d48a0dfbeb
Emerson <emerson1994vp@gmail.com>
2024-04-11 17:21:47 -0300

    c

--- src/services/ListarTudoModal.js
+++ src/services/ListarTudoModal.js
@@ -4,8 +4,8 @@ module.exports = {
     listarTudo: (nome) => {
         return new Promise((aceito, rejeitado)=>{
             db.query('SELECT j.nome as Jogador, t.nome as Time, l.nome As Liga FROM jogadores j \
-            left join times_tb t on j.id_time = t.id_time left join ligas l \
-            on t.id_liga = l.id ', (error, results) => {
+            left join times_tb t on j.id_time = t.id_time left join ligas l on t.id_liga = l.id \
+            left join artilheiro a on a.id_jogador = j.id_jogador', (error, results) => {
                 if(error) { rejeitado(error); return; }
                //vai verificar se retornou mais de 1 e pegar o 1
                     aceito(results);    
